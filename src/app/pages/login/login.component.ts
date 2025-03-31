0eb6e7558ac5ed5f5c0ee8e0b7194777520108df
jadeilton <jadeiltondm@gmail.com>
2025-03-30 21:47:44 -0300

    criei o inicio

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { DafaultLoginLayoutComponent } from '../../components/dafault-login-layout/dafault-login-layout.component';
+
+
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [DafaultLoginLayoutComponent],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss'] // Corrigido o nome da propriedade
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
